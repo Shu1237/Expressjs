1328969a560aad41ff75298a31b612e26222b31f
Shu1237 <ttei8191@gmail.com>
2025-05-21 08:14:49 +0700

    Update congfig.js

--- src/congfig.js
+++ src/congfig.js
@@ -13,7 +13,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
-  'http://localhost:3001'
+  'http://localhost:3001',
+  'https://mock2-w.vercel.app'
 ];
 
 export const corOption = {
